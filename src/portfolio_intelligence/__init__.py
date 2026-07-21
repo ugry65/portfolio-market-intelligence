288ad7d6f50b66314049d80cadfc1cd332a34409
ugry65 <olahimre65@gmail.com>
2026-07-21 19:44:16 +0200

    Add package initializer

---
+++ src/portfolio_intelligence/__init__.py
@@ -0,0 +1,5 @@
+"""Portfolio Market Intelligence backend."""
+
+from .models import Evidence, PortfolioImpact, Signal, SourceStatus
+
+__all__ = ["Evidence", "PortfolioImpact", "Signal", "SourceStatus"]
